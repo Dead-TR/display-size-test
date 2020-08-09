2fbd705e7ca6afbdc3bf733ffbda692b8b00bd9b
DTR <t.terry.rice@gmail.com>
2020-08-09 20:18:01 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,8 @@
 const header = document.querySelector('.header');
 const widthTextElement = document.querySelector('.width');
 const heightTextElement = document.querySelector('.height');
-widthTextElement.innerText = `Width = ${document.body.offsetWidth}px`;
-heightTextElement.innerText = `Height = ${document.body.offsetHeight}px`;
+
+setTimeout(() => {
+  widthTextElement.innerText = `Width = ${document.body.offsetWidth}px`;
+  heightTextElement.innerText = `Height = ${document.body.offsetHeight}px`;
+}, 500);
